1fc383a992a3e8ac382271e6451afe084346a523
TaeHyung <soul.childhc@gmail.com>
2021-01-07 14:28:12 +0900

    add gitignore

--- pharm_profile/app.py
+++ pharm_profile/app.py
@@ -47,5 +47,32 @@ def save_profile():
 #     return jsonify({'result': 'success', 'orders': order_list})
 
 
+
+## HTML 화면 보여주기
+@app.route('/reviews')
+def reviews():
+    return render_template('index_review.html')
+
+
+# 주문하기(POST) API
+@app.route('/reviews', methods=['POST'])
+def save_profile():
+    phone_number_receive = request.form['phone_number_give']
+    pharm_name_receive = request.form['pharm_name_give']
+    star_give_receive = request.form['star_give']
+    review_detail_receive = request.form['review_detail_give']
+
+    reviews = {
+        'phone_number' : phone_number_receive,
+        'pharm_name' : pharm_name_receive,
+        'star_give' : star_give_receive,
+        'review_detail' : review_detail_receive,
+    }
+
+    db.reviews.insert_one(reviews)
+
+    return jsonify({'result': 'success', 'msg' : '후기등록이 완료되었습니다!'})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
